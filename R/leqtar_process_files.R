681f4928faeffff0eeb110aba69051ee2b4f72cb
ErikSchutte <schutte.erik@hotmail.com>
2017-03-15 20:00:48 +0100

    [v0.1.0] Added file reading, try-catch handling. Note: Fix the unique row names issue by adding numeric identifiers to each row.

--- R/leqtar_process_files.R
+++ R/leqtar_process_files.R
@@ -15,69 +15,27 @@
 leqtar_process_files <- function(arguments) {
 
   # Extract file extensions --------------------
-  message("\n[INFO] Determine extensions..")
+  message("[INFO] Determine extensions..")
   genotype_file_extension <- unlist(str_split(arguments$genotype, "\\."))[2]
   expression_file_extension <- unlist(str_split(arguments$expression, "\\."))[2]
   if ( !is.null(arguments$covariates) ) {
-    covariates_file_extension <- unlist(str_split(arguments$covarites, "\\."))[2]
+    covariates_file_extension <- unlist(str_split(arguments$covariates, "\\."))[2]
   }
-  message("\n[INFO] File extensions determined..")
+  message("[INFO] File extensions determined..")
+
   # Determine read methods.
-  message("\n[INFO] Reading files..")
+  message("[INFO] Reading files..")
   # genotype --------------------
-  if (genotype_file_extension == "txt") {
-    genotype_file_content <- read.table(arguments$genotype, stringsAsFactors = F, header = T, sep="\t")
-  } else if (genotype_file_extension == "Rdata" | genotype_file_extension == "RData") {
-    tmp_env <- new.env()
-    load( arguments$genotype, tmp_env )
-    genotype_file_extension = get( ls( tmp_env )[1], envir=tmp_env )
-    rm(tmp_env)
-  }else if (genotype_file_extension == "xlsx") {
-    genotype_file_content <- read.xls(arguments$genotype)
-  } else if (genotype_file_extension == "csv") {
-    genotype_file_content <- read.csv(arguments$genotype, header = T, sep=",")
-  } else if (genotype_file_extension == "tsv") {
-    genotype_file_content <- read.table(arguments$genotype, stringsAsFactors = F, header = T, sep="\t")
-  } else {
-    stop("[STOP] File extension: ", genotype_file_extension, " not supported!\nCurrently supports txt, xlsx, csv and tsv files.")
-  }
+  genotype_file_content <- read_files(genotype_file_extension, arguments$genotype)
+
   # expression -------------------
-  if (expression_file_extension == "txt") {
-    expression_file_content <- read.table(arguments$expression, stringsAsFactors = F, header = T, sep="\t")
-  } else if (expression_file_content == "Rdata" | expression_file_content == "RData") {
-    tmp_env <- new.env()
-    load( arguments$expression, tmp_env )
-    expression_file_content = get( ls( tmp_env )[1], envir=tmp_env )
-    rm(tmp_env)
-  } else if (expression_file_extension == "xlsx") {
-    expression_file_content <- read.xls(arguments$expression)
-  } else if (expression_file_extension == "csv") {
-    expression_file_content <- read.csv(arguments$expression, header = T, sep=",")
-  } else if (expression_file_extension == "tsv") {
-    expression_file_content <- read.table(arguments$expression, stringsAsFactors = F, header = T, sep="\t")
-  } else {
-    stop("[STOP] File extension: ", expression_file_extension, " not supported!\nCurrently supports txt, xlsx, csv and tsv files.")
-  }
+  expression_file_content <- read_files(expression_file_extension, arguments$expression)
+
   # covariates -------------------
   if ( !is.null(arguments$covariates) ) {
-    if (covariates_file_extension == "txt") {
-      covariates_file_content <- read.table(arguments$covariates, stringsAsFactors = F, header = T, sep="\t")
-    } else if (covariates_file_extension == "Rdata" | covariates_file_extension == "RData") {
-      tmp_env <- new.env()
-      load( arguments$covariates, tmp_env )
-      covariates_file_content = get( ls( tmp_env )[1], envir=tmp_env )
-      rm(tmp_env)
-    } else if (covariates_file_extension == "xlsx") {
-      covariates_file_content <- read.xls(arguments$covariates)
-    } else if (covariates_file_extension == "csv") {
-      covariates_file_content <- read.csv(arguments$covariates, header = T, sep=",")
-    } else if (covariates_file_extension == "tsv") {
-      covariates_file_content <- read.table(arguments$covariates, stringsAsFactors = F, header = T, sep="\t")
-    } else {
-      stop("[STOP] File extension: ", covariates_file_extension, " not supported!\nCurrently supports txt, xlsx, csv and tsv files.")
-    }
+    covariate_file_content <- read_files(covariate_file_extension, arguments$covariates)
   }
-  message("\n[INFO] Files read..")
+  message("[INFO] Files read..")
 
   # Check dimensions -------------------
   message("\n[INFO] Checking dimensions..")
@@ -98,15 +56,47 @@ leqtar_process_files <- function(arguments) {
       stop("[STOP] Dimension of your genotype and expression files do not match!")
     }
   }
-  message("\n[INFO] Dimensions match..")
+  message("[INFO] Dimensions match..")
 
   # Check column name order ---------------------------------
-  message("\n[INFO] Checking column names..")
+  message("[INFO] Checking column names..")
   genotype_colnames <- colnames(genotype_file_content)
   expression_colnames <- colnames(expression_file_content)
   if ( !is.null(arguments$covariates) ) {
     covariates_colnames <- colnames(covariates_file_content)
   }
+}
 
-
+read_files <- function(file_extension, file_path) {
+  file_content <- tryCatch({
+    message("[INFO] File extension: ", file_extension, "\n[INFO] File path: ", file_path)
+    if (file_extension == "txt") {
+      read.table(file_path, stringsAsFactors = F, header = T, sep="\t")
+    } else if (file_extension == "Rdata" | file_extension == "RData") {
+      tmp_env <- new.env()
+      load( file_path, tmp_env )
+      file_content = get( ls( tmp_env )[1], envir=tmp_env )
+      rm(tmp_env)
+    } else if (file_extension == "xlsx" | file_extension == "xls") {
+      read.xls(file_path)
+    } else if (file_extension == "csv") {
+      read.csv(file_path, header = T, sep=",")
+    } else if (file_extension == "tsv") {
+      read.table(file_path, stringsAsFactors = F, header = T, sep="\t")
+    } else {
+      stop("[STOP] File extension: ", file_extension, " not supported!\nCurrently supports txt, xlsx, csv and tsv files.")
+    }
+  },
+  error=function(condition) {
+    message("[ERR] Could not read in file, critical error..\n[ERR] Reason:\n[ERR] ", condition)
+    return(NULL)
+  },
+  warning=function(condition) {
+    message("[WARNING] Could not read in file..\n[WARNING] Reason:\n[WARNING] ", condition)
+    return(NULL)
+  },
+  finally={
+    message("[INFO] File: ", file_path, " read correctly..")
+  })
+  return(file_content)
 }
