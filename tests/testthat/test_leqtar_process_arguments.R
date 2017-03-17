871b99bc3f43d636a236f3339c9d799c410cc4d0
Molgenis <molgenis@Molgeniss-MacBook-Pro.local>
2017-03-17 17:14:20 +0100

    [v0.1.0] Updated documentation, tweaked some unit tests.

--- tests/testthat/test_leqtar_process_arguments.R
+++ tests/testthat/test_leqtar_process_arguments.R
@@ -1,11 +1,19 @@
 library(leqtar)
+library(testthat)
 context("Testing input arguments are valid arguments per leqtar's input standard..")
 
-# Set files
+# Set files manually
+genotypeFile = file.path("data", "genotype_test_data.RData", fsep=.Platform$file.sep)
+expressionFile = file.path("data", "expression_test_data.RData", fsep=.Platform$file.sep)
+covariateFile = file.path("data", "covariate_test_data.RData", fsep=.Platform$file.sep)
+output_dir = file.path(getwd())
+genoToFreq = F
+
+# Set files unittest
 genotypeFile = file.path("..", "..", "data", "genotype_test_data.RData", fsep=.Platform$file.sep)
 expressionFile = file.path("..", "..", "data", "expression_test_data.RData", fsep=.Platform$file.sep)
 covariateFile = file.path("..", "..", "data", "covariate_test_data.RData", fsep=.Platform$file.sep)
-output_dir = "~"
+output_dir = file.path(getwd())
 genoToFreq = F
 
 # Test case 1 -----------------------------
@@ -18,14 +26,14 @@ test_that("arguments are correctly processed when parsing a genotypeFile and an 
   # Test if expression data is loaded correctly.
   expect_true( file.exists( file.path(arguments$expression) ), info = "Note: expression file should exist in this scenario." )
   # Test if the output folder is created.
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar_out should exist in this scenario" )
   # Test if the genoToFreq flag works.
-  expect_equal(arguments$genoToFreq, FALSE, info = "Note: genoToFreq should be FALSE in this scenario")
+  expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
   # Test if the process_arguments function validated the arguments.
   expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
-  # unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
+  unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
 })
 
 # Test case 2 -----------------------------
@@ -40,14 +48,14 @@ test_that("arguments are correctly processed when parsing a genotypeFile, expres
   # Test if covariate data is loaded correctly.
   expect_true( file.exists( file.path(arguments$covariate) ), info = "Note: covariate file should exist in this scenario." )
   # Test if the output folder is created.
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar_out should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
   # Test if the process_arguments function validated the arguments.
   expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
-  # unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
+  unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
 })
 
 # Test case 3 ----------------------------
@@ -62,19 +70,19 @@ test_that("arguments are correctly processed when parsing a genotypeFile, an exp
   # Test if covariate data is loaded correctly.
   expect_true( file.exists( file.path(arguments$covariate) ), info = "Note: covariate file should exist in this scenario." )
   # Test if the output folder is created.
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
   # Test if the output variable is set & created.
   expect_true( dir.exists( file.path(arguments$output) ), info = "Note: leqtar should exist in this scenario" )
   # Test if the subdirectories for the output directory is created
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", "images", fsep=.Platform$file.sep) ), info = "Note: leqtar/images should exist in this scneario" )
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", "data", fsep=.Platform$file.sep) ), info = "Note: leqtar/data should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, "images", fsep=.Platform$file.sep) ), info = "Note: leqtar_out/images should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, "data", fsep=.Platform$file.sep) ), info = "Note: leqtar_out/data should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
   # Test if the process_arguments function validated the arguments.
   expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
-  # unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
+  unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T )
 })
 
 # Test case 4 ----------------------------
@@ -89,18 +97,18 @@ test_that("arguments are correctly processed when parsing a genotypeFile, an exp
   # Test if covariate data is loaded correctly.
   expect_true( file.exists( file.path(arguments$covariate) ), info = "Note: covariate file should exist in this scenario." )
   # Test if the output folder is created.
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
   # Test if the output variable is set & created.
   expect_true( dir.exists( file.path(arguments$output) ), info = "Note: leqtar should exist in this scenario" )
   # Test if the subdirectories for the output directory is created
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", "images", fsep=.Platform$file.sep) ), info = "Note: leqtar/images should exist in this scneario" )
-  expect_true( dir.exists( file.path(arguments$output, "leqtar", "data", fsep=.Platform$file.sep) ), info = "Note: leqtar/data should exist in this scenario" )
+  expect_true( dir.exists( file.path(arguments$output, "images", fsep=.Platform$file.sep) ), info = "Note: leqtar_out/images should exist in this scneario" )
+  expect_true( dir.exists( file.path(arguments$output, "data", fsep=.Platform$file.sep) ), info = "Note: leqtar_out/data should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
   # Test if the process_arguments function validated the arguments.
   expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
-  # unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
+  unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
 })
 
