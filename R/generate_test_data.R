09b08620c23fce8220c5e1b4a465be131c87ba8e
Molgenis <molgenis@client-145-120-36-170.surfnet.eduroam.rug.nl>
2017-03-20 17:55:56 +0100

    [v0.1.0] Updated documentation, added checks for genotype value recognition, re-created the data per CRAN check's suggestion and modified a unittest.

--- R/generate_test_data.R
+++ R/generate_test_data.R
@@ -1,7 +1,6 @@
 #' Generate test data
 #'
 #' Generates test data files, mainly for debugging and testing.
-#' @import 'random'
 generate_test_data <- function() {
 
   test_sample_names <- lapply(seq(101, 300, 1), function(i) {
@@ -31,27 +30,27 @@ snps_geno <- sapply(test_cases$test_snp_names, function(snp) {
 
 genotype_test_data <- data.frame(snps_geno)
 rownames(genotype_test_data) <- test_cases$test_sample_names
-dim(genotype_test_data)
-length(test_cases$test_snp_names)
-length(test_cases$test_sample_names)
-head(genotype_test_data)
+# dim(genotype_test_data)
+# length(test_cases$test_snp_names)
+# length(test_cases$test_sample_names)
+# head(genotype_test_data)
 
 # Generate expression data.
 expression_test_data <- matrix(rbinom(10*1000, 3000, .5), ncol=200, nrow=5000)
 rownames(expression_test_data) <- test_cases$test_gene_names
 colnames(expression_test_data) <- test_cases$test_sample_names
-head(expression_test_data)
-tail(expression_test_data)
-plot(density(expression_test_data))
+# head(expression_test_data)
+# tail(expression_test_data)
+# plot(density(expression_test_data))
 
 # Generate covariates.
 covariate_test_data <- t(data.frame( replicate(200, sample(18:75, 1, rep=T) ), replicate(200, sample(0:1, 1, rep=T) ) ))
 colnames(covariate_test_data) <- test_cases$test_sample_names
 rownames(covariate_test_data) <- c("age", "gender")
-head(covariate_test_data)
+# head(covariate_test_data)
 
 # Save data.
-# save(genotype_test_data, file="~/git/leqtar/data/genotype_test_data.Rdata")
-# save(expression_test_data, file="~/git/leqtar/data/expression_test_data.Rdata")
-# save(covariate_test_data, file="~/git/leqtar/data/covariate_test_data.Rdata")
+# save(genotype_test_data, file="~/git/leqtar/data/genotype_test_data.RData")
+# save(expression_test_data, file="~/git/leqtar/data/expression_test_data.RData")
+# save(covariate_test_data, file="~/git/leqtar/data/covariate_test_data.RData")
 
