9da69a65aeacedf461682143cd31637bd442d4fa
ErikSchutte <schutte.erik@hotmail.com>
2017-04-04 14:39:00 +0200

    [v0.1.0] New location data sets for genes and snps. Added documentation, wrote new uinttests and a lot of tweaks and some restructering.

--- R/leqtar_create_tables.R
+++ R/leqtar_create_tables.R
@@ -2,7 +2,6 @@
 #' leqtar_create_tables
 #'
 #' Creates result tables from the data file produced by leqtar_analysis.
-#' @param somePath change this later.
 leqtar_create_tables <- function() {
   message("[INFO] ----------#----------")
   message("[INFO] Creating result tables..")
--- tests/testthat/test_leqtar_process_arguments.R
+++ tests/testthat/test_leqtar_process_arguments.R
@@ -9,64 +9,64 @@ root_file <- root$make_fix_file()
 
 # Set arguments.
 genotypeFile <- root_file("test_data", "genotype_test_data.RData")
-expressionFile <- root_file("test_data", "expression_test_data.RData")
+genotypePositionFile <- root_file("test_data", "genotype_test_locations.RData")
+phenotypeFile <- root_file("test_data", "phenotype_test_data.RData")
+phenotypePositionFile <- root_file("test_data", "phenotype_test_locations.RData")
 covariateFile <- root_file("test_data", "covariate_test_data.RData")
 output_dir <- file.path( getwd() )
-genoToFreq = F
+useModel <- "linear"
+genoToFreq <- FALSE
 
 # Test case 1 -----------------------------
-test_that("arguments are correctly processed when parsing a genotypeFile and an expressionFile..", {
-  arguments <- leqtar:::process_arguments(genotypeFile = genotypeFile, expressionFile = expressionFile,
-                                          covariateFile = NULL, output_dir = NULL, genoToFreq)
+test_that("arguments are correctly processed when parsing a genotypeFile and an phenotypeFile..", {
+  arguments <- leqtar:::process_arguments(run_name = "test_case_01", genotypeFile = genotypeFile, genotypePositionFile = genotypePositionFile,
+                                          phenotypeFile = phenotypeFile, phenotypePositionFile = phenotypePositionFile, useModel = useModel,
+                                          covariateFile = NULL, output_dir = NULL, genoToFreq, forceRun = F)
 
   # Test if genotype data is loaded correctly.
   expect_true( file.exists( file.path(arguments$genotype) ), info = "Note: genotype file should exist in this scenario." )
-  # Test if expression data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$expression) ), info = "Note: expression file should exist in this scenario." )
+  # Test if phenotype data is loaded correctly.
+  expect_true( file.exists( file.path(arguments$phenotype) ), info = "Note: phenotype file should exist in this scenario." )
   # Test if the output folder is created.
   expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar_out should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
-  # Test if the process_arguments function validated the arguments.
-  expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
   unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
 })
 
 # Test case 2 -----------------------------
-test_that("arguments are correctly processed when parsing a genotypeFile, expressionFile and a covariatesFile", {
-  arguments <- leqtar:::process_arguments(genotypeFile = genotypeFile, expressionFile = expressionFile,
-                                          covariateFile = covariateFile, output_dir = NULL, genoToFreq)
-
+test_that("arguments are correctly processed when parsing a genotypeFile, phenotypeFile and a covariatesFile", {
+  arguments <- leqtar:::process_arguments(run_name = "test_case_02", genotypeFile = genotypeFile, genotypePositionFile = genotypePositionFile,
+                                          phenotypeFile = phenotypeFile, phenotypePositionFile = phenotypePositionFile, useModel = useModel,
+                                          covariateFile = covariateFile, output_dir = NULL, genoToFreq, forceRun = F)
   # Test if genotype data is loaded correctly.
   expect_true( file.exists( file.path(arguments$genotype) ), info = "Note: genotype file should exist in this scenario." )
-  # Test if expression data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$expression) ), info = "Note: expression file should exist in this scenario." )
+  # Test if phenotype data is loaded correctly.
+  expect_true( file.exists( file.path(arguments$phenotype) ), info = "Note: phenotype file should exist in this scenario." )
   # Test if covariate data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$covariate) ), info = "Note: covariate file should exist in this scenario." )
+  expect_true( file.exists( file.path(arguments$covariates) ), info = "Note: covariate file should exist in this scenario." )
   # Test if the output folder is created.
   expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar_out should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
-  # Test if the process_arguments function validated the arguments.
-  expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
   unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
 })
 
 # Test case 3 ----------------------------
-test_that("arguments are correctly processed when parsing a genotypeFile, an expressionFile, a covariatesFile and a specific output directory", {
-  arguments <- leqtar:::process_arguments(genotypeFile = genotypeFile, expressionFile = expressionFile,
-                                          covariateFile = covariateFile, output_dir = output_dir, genoToFreq)
-
+test_that("arguments are correctly processed when parsing a genotypeFile, an phenotypeFile, a covariatesFile and a specific output directory", {
+  arguments <- leqtar:::process_arguments(run_name = "test_case_03", genotypeFile = genotypeFile, genotypePositionFile = genotypePositionFile,
+                                          phenotypeFile = phenotypeFile, phenotypePositionFile = phenotypePositionFile, useModel = useModel,
+                                          covariateFile = covariateFile, output_dir = output_dir, genoToFreq, forceRun = F)
   # Test if genotype data is loaded correctly.
   expect_true( file.exists( file.path(arguments$genotype) ), info = "Note: genotype file should exist in this scenario." )
-  # Test if expression data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$expression) ), info = "Note: expression file should exist in this scenario." )
+  # Test if phenotype data is loaded correctly.
+  expect_true( file.exists( file.path(arguments$phenotype) ), info = "Note: phenotype file should exist in this scenario." )
   # Test if covariate data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$covariate) ), info = "Note: covariate file should exist in this scenario." )
+  expect_true( file.exists( file.path(arguments$covariates) ), info = "Note: covariate file should exist in this scenario." )
   # Test if the output folder is created.
   expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
   # Test if the output variable is set & created.
@@ -76,24 +76,22 @@ test_that("arguments are correctly processed when parsing a genotypeFile, an exp
   expect_true( dir.exists( file.path(arguments$output, "data", fsep=.Platform$file.sep) ), info = "Note: leqtar_out/data should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
-  # Test if the process_arguments function validated the arguments.
-  expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
   unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T )
 })
 
 # Test case 4 ----------------------------
-test_that("arguments are correctly processed when parsing a genotypeFile, an expressionFile, a covariatesFile, a specific output directory and the genoToFreq flag", {
-  arguments <- leqtar:::process_arguments(genotypeFile = genotypeFile, expressionFile = expressionFile,
-                                          covariateFile = covariateFile, output_dir = output_dir, genoToFreq)
-
+test_that("arguments are correctly processed when parsing a genotypeFile, an phenotypeFile, a covariatesFile, a specific output directory and the genoToFreq flag", {
+  arguments <- leqtar:::process_arguments(run_name = "test_case_04", genotypeFile = genotypeFile, genotypePositionFile = genotypePositionFile,
+                                          phenotypeFile = phenotypeFile, phenotypePositionFile = phenotypePositionFile, useModel = useModel,
+                                          covariateFile = covariateFile, output_dir = output_dir, genoToFreq, forceRun = F)
   # Test if genotype data is loaded correctly.
   expect_true( file.exists( file.path(arguments$genotype) ), info = "Note: genotype file should exist in this scenario." )
-  # Test if expression data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$expression) ), info = "Note: expression file should exist in this scenario." )
+  # Test if phenotype data is loaded correctly.
+  expect_true( file.exists( file.path(arguments$phenotype) ), info = "Note: phenotype file should exist in this scenario." )
   # Test if covariate data is loaded correctly.
-  expect_true( file.exists( file.path(arguments$covariate) ), info = "Note: covariate file should exist in this scenario." )
+  expect_true( file.exists( file.path(arguments$covariates) ), info = "Note: covariate file should exist in this scenario." )
   # Test if the output folder is created.
   expect_true( dir.exists( file.path(arguments$output, fsep=.Platform$file.sep) ), info = "Note: leqtar should exist in this scenario" )
   # Test if the output variable is set & created.
@@ -103,8 +101,6 @@ test_that("arguments are correctly processed when parsing a genotypeFile, an exp
   expect_true( dir.exists( file.path(arguments$output, "data", fsep=.Platform$file.sep) ), info = "Note: leqtar_out/data should exist in this scenario" )
   # Test if the genoToFreq flag works.
   expect_false(arguments$genoToFreq, info = "Note: genoToFreq should be FALSE in this scenario")
-  # Test if the process_arguments function validated the arguments.
-  expect_true(arguments$valid, info = "Note: valid should be TRUE in this scenario")
 
   # clean-up directories.
   unlink( file.path(arguments$output, fsep=.Platform$file.sep), recursive = T)
